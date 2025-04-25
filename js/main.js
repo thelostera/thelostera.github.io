69c03686af8ae926b8dea74514f7dc94aa1fa881
Samuel Austin <samuelaustin@isabelles-MacBook.local>
2025-04-25 15:27:31 -0400

    added js for music button

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+
+const playButton = document.getElementById("music-button");
+// Load our song
+let song = new Audio("assets/sounds/side_a.wav");
+// Get the play button so we can talk to it
+const playButton = document.getElementById("music-button");
+// Tell our button what to do when we click it
+playButton.addEventListener("click", toggleMusic);
+
+// Toggles the music
+function toggleMusic() {
+    // Check if the song is paused
+    if (song.paused) {
+        // If it is, then play it
+        song.play();
+    }
+    else {
+        // If it isn't, then pause it
+        song.pause();
+    }
+}
